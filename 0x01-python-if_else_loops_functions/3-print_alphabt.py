5285caf85a4c14b804070d2da4f3c78b43e066f7
Tevin Deja <tevinowena@gmail.com>
2023-11-28 05:22:59 -0800

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for letters in range(97, 123):
-    if chr(letters) != 'q' and chr(letters) != 'e':
-        print(f"{chr(letters)}", end="")
+for char_code in range(ord('a'), ord('z') + 1):
+    current_char = chr(char_code)
+    if current_char != 'q' and current_char != 'e':
+        print(current_char, end='')
