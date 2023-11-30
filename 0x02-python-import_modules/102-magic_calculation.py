9fd094e6bea9f094bfe5f00ba5532572896b1163
Tevin Deja <tevinowena@gmail.com>
2023-11-30 06:41:41 -0800

    ByteCode -> Python #3

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 
-def custom_calculation(x, y):
+def magic_calculation(x, y):
     from magic_calculation_102 import add, sub
-
     if x < y:
         result = add(x, y)
-
-        for i in range(4, 6):
-            result = add(result, i)
-
-        return result
+        for n in range(4, 6):
+            result = add(result, n)
+            return result
     else:
         return sub(x, y)
