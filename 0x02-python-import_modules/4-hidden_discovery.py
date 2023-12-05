d8cbaa6e8132454e5a23994ae8969ab86623702a
Tevin Deja <tevinowena@gmail.com>
2023-12-05 09:17:57 -0800

    Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,21 +1,9 @@
 #!/usr/bin/python3
-import importlib.util
-import sys
+if __name__ == "__main__":
+    import hidden_4
 
-if sys.version_info < (3, 8):
-    print("This program requires Python 3.8+")
-    sys.exit(1)
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
 
-spec = importlib.util.spec_from_file_location("hidden_4", "hidden_4.pyc")
-module = importlib.util.module_from_spec(spec)
-spec.loader.exec_module(module)
-
-names = []
-for name in dir(module):
-    if not name.startswith('__'):
-        names.append(name)
-
-names.sort()
-
-for name in names:
-    print(name)
