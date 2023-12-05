96e31db44434680c5388eb43a435ddaf8c35d427
Tevin Deja <tevinowena@gmail.com>
2023-12-05 09:14:53 -0800

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys, math
+    import sys
 
     answer = 0
-    for n in sys.argv:
-        answer += int(n)
-        print("{}".format(answer))
+    for n in range(len(sys.argv) - 1):
+        answer += int(sys.argv[n + 1])
+    print("{}".format(answer))
