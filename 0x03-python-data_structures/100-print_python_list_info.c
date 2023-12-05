0bb4a74015daf9ca397363d3db887c5ad05129fa
Tevin Deja <tevinowena@gmail.com>
2023-12-05 08:50:29 -0800

    CPython #0: Python lists

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,22 +1,16 @@
 #include <Python.h>
-#include <stdio.h>
 
-void print_python_list_info(PyObject *p) {
-    if (!PyList_Check(p)) {
-        printf("The given object is not a Python list.\n");
-        return;
-    }
+/**
+ * print_python_list_info - Prints basic info about Python lists
+ * @p: A PyObject list
+ */
+void print_python_list_info(PyObject *p) 
+{
+    PyListObject *list = (PyListObject*)p;
+    int size = PyList_Size(list);
+    int allocated = list->allocated;
 
-    printf("Python list information:\n");
-    printf(" Total elements: %ld\n", PyList_Size(p));
-
-    PyObject *iter = PyObject_GetIter(p);
-    PyObject *item;
-    while ((item = PyIter_Next(iter))) {
-        printf(" List item: ");
-        PyObject_Print(item, stdout, 0);
-        printf("\n");
-        Py_DECREF(item);
-    }
-    Py_DECREF(iter);
+    printf("[*] Size of the Python List = %d\n", size);
+    printf("[*] Allocated = %d\n", allocated);
+    printf("[*] Type object address in memory = %p\n", (void *)list->ob_type);
 }
