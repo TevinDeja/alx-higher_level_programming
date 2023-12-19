63e28af9c1d6f3d62be3958eaf2c7abf0c6c40df
Tevin Deja <tevinowena@gmail.com>
2023-12-19 10:51:42 -0800

    Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines Square based on 1-square.py
+"""
+
+class Square:
+    """Represent square with private size attribute
+    
+    Attributes:
+        __size (int): size of the square
+    
+    """
+
+    def __init__(self, size=0):
+        """Initialize square with optional size
+        
+        Args:
+            size (int, optional): size of square side. Defaults to 0.
+        
+        Raises:
+            TypeError: If size is not int
+            ValueError: If size is less than 0
+         
+        """
+        self.__size = size
+        
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
