32707092be37b80b2c5d2242a5cc70dfdade483c
Tevin Deja <tevinowena@gmail.com>
2023-12-19 10:49:13 -0800

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Defines a class Square with size attribute
+"""
+
+class Square:
+    """Represent a square with private size attribute
+    
+    Attributes:
+        __size (int): The size of the square (instantiated)
+        
+    """
+    
+    def __init__(self, size):
+        """
+        Instantiate a Square with size
+        
+        Args:
+            size (int): The size of the square
+        
+        Returns: None
+            
+        """
+        self.__size = size
