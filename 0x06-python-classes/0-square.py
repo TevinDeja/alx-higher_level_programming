0ef1023556122678e83b4f5b778728af9a48f023
Tevin Deja <tevinowena@gmail.com>
2023-12-19 10:46:22 -0800

    My first square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a Square class 
+"""
+
+class Square:
+    """Represents a square
+    
+    Attributes:
+        None 
+    """
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,4 @@
+Python - Classes and Objects
+Python
+OOP
+6 Mandatory Tasks and 4 Advanced Tasks
