aabe01f344d55bd82a4f2e301a79888be8ff0a2c
Tevin Deja <tevinowena@gmail.com>
2024-01-04 04:32:58 -0800

    Change representation

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -6,10 +6,10 @@ class Rectangle:
     """Represent a rectangle"""
     number_of_instances = 0
     print_symbol = '#'
-    
+
     def __init__(self, width=0, height=0):
         """Initialize new Rectangle"""
-        self.width = width 
+        self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
 
@@ -18,7 +18,7 @@ class Rectangle:
         """Get the width of the Rectangle"""
         return self.__width
 
-    @width.setter 
+    @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
@@ -32,17 +32,17 @@ class Rectangle:
         return self.__height
 
     @height.setter
-    def height(self, value): 
+    def height(self, value):
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-        
+
     def area(self):
         """Return the area of the Rectangle"""
         return self.__width * self.__height
-                              
+
     def perimeter(self):
         """Return the perimeter of the Rectangle"""
         if self.__width == 0 or self.__height == 0:
@@ -64,5 +64,5 @@ class Rectangle:
 
     def __del__(self):
         """Print a message for every deletion of a Rectangle"""
-        Rectangle.number_of_instances -= 1 
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
