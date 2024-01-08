7ed58c975d5203eb62a0462384f75594925806ea
Tevin Deja <tevinowena@gmail.com>
2024-01-08 04:28:12 -0800

    Exact same object

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a class checking function"""
+
+def is_same_class(obj, a_class):
+    """Check if an object is an instance of a given class"""
+    if type(obj) is a_class:
+        return True
+    return False
