2ece284690e43a6c225867562be33fbe06f79da3
Tevin Deja <tevinowena@gmail.com>
2024-01-08 05:41:00 -0800

    Lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-"""Defines an object attribute lookup function."""
+"""Defines an object attribute lookup function"""
+
 
 def lookup(obj):
-    """Return a list of an object's available attributes."""
-    result = []
-    for i in dir(obj):
-        if not i.startswith('__'):
-            result.append(i)
-    return result
+    """Return a list of the availabe object's attributes"""
+    return (dir(obj))
