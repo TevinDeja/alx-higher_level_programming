f909ef92e3bd3826bff3c7be7691006433db5f65
Tevin Deja <tevinowena@gmail.com>
2024-01-09 08:26:27 -0800

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,25 +1,15 @@
 #!/usr/bin/env python3
-
+"""Add all arguments to a Python list"""
 import sys
-from typing import List
-from collections.abc import Iterable
-from pathlib import Path
-from import dumps
-from json import
-from json import dump
-from json import
 
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-def save_to_json_file(my_obj: Iterable, filename: str) -> None:
-   """Serializes `my_obj` to a JSON file with the name `filename`."""
-   filepath = Path(filename)
-   filepath.touch(exist_ok=True)
-   with filepath.open('w', encoding='utf-8') as f:
-       for item in my_obj:
-           dump(item, f)
-
-
-def load_from_json_file(filename: str) -> List[str]:
-   """Deserializes a JSON file with the name `filename` to a list of strings."""
-   filepath = Path(filename)
-   if not filepath.exists():
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
