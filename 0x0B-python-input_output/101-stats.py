1f414b3c4702669cae364bf3ec030fcaed8447bb
Tevin Deja <tevinowena@gmail.com>
2024-01-10 04:59:26 -0800

    Log parsing

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,22 +1,47 @@
 #!/usr/bin/python3
 """Reads from standard input and computes metrics"""
-import sys
 
-total_size = 0
-status_codes = {}
+def print_stats(size, status_codes):
+    """Print accumulated metrics """
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
 
-for line_num, line in enumerate(sys.stdin, 1):
-    line = line.split()
-    total_size += int(line[-1])
 
-    status_code = line[-2]
-    if status_code in status_codes:
-        status_codes[status_code] += 1
-    else:
-        status_codes[status_code] = 1
+if __name__ == "__main__":
+    import sys
 
-    if line_num % 10 == 0:
-        print("File size: {}".format(total_size))
-        for code in sorted(status_codes.keys()):
-            if status_codes[code] > 0:
-                print("{}: {}".format(code, status_codes[code]))
+    size = 0
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
+
+    try:
+        for line in sys.stdin:
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
+            else:
+                count += 1
+
+            line = line.split()
+
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(size, status_codes)
+        raise
