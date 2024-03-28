24fdc71a9953e2fd028f350b0e20dbd90d0b5ab7
TevinDeja <tevinowena@gmail.com>
2024-03-28 02:20:55 -0700

    What's my status?

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Script that fetches https://alx-intranet.hbtn.io/status"""
+
+import urllib.request
+
+def fetch_status():
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- {}".format(body.decode('utf-8')))
+
+if __name__ == "__main__":
+    fetch_status()
