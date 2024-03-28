931d174e46cf9cbc2818cc11ec244421b4767ebe
TevinDeja <tevinowena@gmail.com>
2024-03-28 03:21:08 -0700

    Response header value

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,12 +2,11 @@
 """Script that fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
+if __name__ == "__main__":
+    fetch_status()
 
-def fetch_status():
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+    def fetch_status():
+        with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
         body = response.read()
         print("Body response:")
         print("\t- {}".format(body.decode('utf-8')))
-
-if __name__ == "__main__":
-    fetch_status()
