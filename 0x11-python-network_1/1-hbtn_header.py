9b32013885026ffd45ea5978dc1c2e08a6774ec7
TevinDeja <tevinowena@gmail.com>
2024-03-28 03:29:10 -0700

    Response header value

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-script that takes in a URL
-sends a request to the URL
+script that takes in a URL sends a request to the URL
 displays the value of the X-Request-Id variable found in the header of response.
 """
 import urllib.request
