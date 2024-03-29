1ab26e2611915806b1c3543b69f4c4dc9fccccaf
TevinDeja <tevinowena@gmail.com>
2024-03-29 01:55:45 -0700

    My GitHub!

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,8 +3,8 @@
 script that takes in a URL sends a request to the URL
 displays the value of the X-Request-Id variable found in the header of response.
 """
-import urllib.request
 import sys
+import urllib.request
 
 
 if __name__ == "__main__":
