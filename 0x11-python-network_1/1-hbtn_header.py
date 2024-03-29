388956f77a0ac626160d5b5b25581835e1b5e818
TevinDeja <tevinowena@gmail.com>
2024-03-29 01:51:10 -0700

    My GitHub!

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,7 +7,9 @@ import urllib.request
 import sys
 
 
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        with urllib.request.urlopen(sys.argv[1]) as response:
-            print(response.getheader('X-Request-Id'))
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -10,17 +10,8 @@ import sys
 if __name__ == '__main__':
     username = sys.argv[1]
     token = sys.argv[2]
-
-    url = 'https://api.github.com/user'
-    headers = {
-        'Authorization': f'token {token}',
-        'Accept': 'application/vnd.github+json',
-    }
-
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
-        user_data = response.json()
-        print(user_data.get('id'))
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
+    r = requests.get('https://api.github.com/user', auth=(username, token))
+    if r.status_code == 200:
+        print(r.json().get('id'))
+    else:
+        print(None)
