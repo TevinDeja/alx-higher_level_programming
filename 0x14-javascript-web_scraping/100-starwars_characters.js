2d8cd2769e3f30c261cffd1940774d761e925317
TevinDeja <teviowena@gmail.com>
2024-04-23 04:35:11 -0700

    Who was playing in this movie?

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const apiUrl = 'https://swapi.dev/api/films/';
+
+function getMovieCharacters(movieId) {
+	const url = `${apiUrl}${movieId}`;
+	request(url, (error, response, body) => {
+		if (!error && response.statusCode === 200) {
+			const movie = JSON.parse(body);
+			const characterUrls = movie.characters;
+
+			characterUrls.forEach(characterUrl => {
+				request(characterUrl, (error, response, body) => {
+					if (!error && response.statusCode === 200) {
+						const character = JSON.parse(body);
+						console.log(character.name);
+					} else {
+						console.error(`Error fetching character: ${error}`);
+					}
+				});
+			});
+		} else {
+			console.error(`Error fetching movie: ${error}`);
+		}
+	});
+};
