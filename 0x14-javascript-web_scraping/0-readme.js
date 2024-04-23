d2878ce40d4853e66251efc908f12f097a237df8
TevinDeja <teviowena@gmail.com>
2024-04-23 04:39:33 -0700

    Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-const fs = require('fs');
-const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf-8', (err, data) => {
-	if (err) {
-		console.error(err);
-	}else{
-		console.log(data);
-	}
-});
+const filesys = require('fs');
+filesys.readFile(process.argv[2], 'utf-8',
+  function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(data);
+  });
